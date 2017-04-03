623c18de8051c0050ca5b6d22f30676d2d779e13
scrolldown <scrolldown@naver.com>
2017-04-04 04:06:19 +0900

    -a

--- fuzzy.py
+++ fuzzy.py
@@ -2,6 +2,109 @@ import crawler
 import calculator
 import pandas
 
+def RuleCalc(propensity):
+    ########################################
+    # Rule evaluation - The rules are different according to user's propensity
+    # propensity 1 - conservative propensity
+    # rule1
+    # IF safety / low
+    # and returns / high
+    # then importance / low
+    #
+    # rule2
+    # IF safety / high
+    # and returns / low
+    # then importance / high
+    #
+    # rule3
+    # IF safety / middle
+    # and returns / high
+    # then importance / middle
+    #########################################
+    if propensity=='conservative':
+        setHigh=[]
+        for index, x in fuzzyDf.iterrows():
+            setHigh.append(x['SafetyHigh']*x['returnLow'])
+        fuzzyDf['setHigh']=setHigh
+
+        setMid=[]
+        for index, x in fuzzyDf.iterrows():
+            setMid.append(x['SafetyMid']*x['returnHigh'])
+        fuzzyDf['setMid']=setMid
+
+        setLow=[]
+        for index, x in fuzzyDf.iterrows():
+            setLow.append(x['SafetyLow']*x['returnHigh'])
+        fuzzyDf['setLow']=setLow
+        return fuzzyDf['setHigh'],fuzzyDf['setMid'],fuzzyDf['setLow']
+    ########################################
+    # propensity 2 - moderate propensity
+    # rule1
+    # IF safety / low
+    # and returns / high
+    # then importance / middle
+    #
+    # rule2
+    # IF safety / high
+    # and returns / low
+    # then importance / low
+    #
+    # rule3
+    # IF safety / middle
+    # and returns / high
+    # then importance / high
+    #########################################
+    elif propensity=='moderate':
+        setHigh=[]
+        for index, x in fuzzyDf.iterrows():
+            setHigh.append(x['SafetyMid']*x['returnHigh'])
+        fuzzyDf['setHigh']=setHigh
+
+        setMid=[]
+        for index, x in fuzzyDf.iterrows():
+            setMid.append(x['SafetyHigh']*x['returnLow'])
+        fuzzyDf['setMid']=setMid
+
+        setLow=[]
+        for index, x in fuzzyDf.iterrows():
+            setLow.append(x['SafetyLow']*x['returnHigh'])
+        fuzzyDf['setLow']=setLow
+        return fuzzyDf['setHigh'],fuzzyDf['setMid'],fuzzyDf['setLow']
+    
+    ########################################
+    # propensity 3 - aggresive propensity
+    # rule1
+    # IF safety / low
+    # and returns / high
+    # then importance / high
+    #
+    # rule2
+    # IF safety / high
+    # and returns / low
+    # then importance / low
+    #
+    # rule3
+    # IF safety / middle
+    # and returns / high
+    # then importance / middle
+    #########################################
+    elif propensity=='aggresive':
+        setHigh=[]
+        for index, x in fuzzyDf.iterrows():
+            setHigh.append(x['SafetyLow']*x['returnHigh'])
+        fuzzyDf['setHigh']=setHigh
+
+        setMid=[]
+        for index, x in fuzzyDf.iterrows():
+            setMid.append(x['SafetyMid']*x['returnHigh'])
+        fuzzyDf['setMid']=setMid
+
+        setLow=[]
+        for index, x in fuzzyDf.iterrows():
+            setLow.append(x['SafetyHigh']*x['returnLow'])
+        fuzzyDf['setLow']=setLow
+        return fuzzyDf['setHigh'],fuzzyDf['setMid'],fuzzyDf['setLow']
+
 domesticDf=crawler.GetDf('http://info.finance.naver.com/fund/fundTypeEarningRate.nhn?ivstAreaWorldYn=N')
 overseasDf=crawler.GetDf('http://info.finance.naver.com/fund/fundTypeEarningRate.nhn?ivstAreaWorldYn=Y')
 
@@ -31,7 +134,7 @@ fuzzyDf=pandas.DataFrame()
 fuzzyDf['대유형']=resultDf['대유형']
 
 ###############################################
-# 퍼지화
+# Fuzzyfication
 # x를 계산하는 함수는 설문조사 혹은 인공지능에 따라 함수 변경 가능
 # x/15-(16/3)같은 부분을 함수화 시켜 차후 DB와 연동.
 ##############################################
@@ -85,42 +188,18 @@ for x in resultDf['기대수익률']:
         returnLow.append(0)
 fuzzyDf['returnLow']=returnLow
 
-########################################
-# 규칙계산 - 고객의 성향에 따라 규칙은 달라짐.
-# 현재규칙 - 보수적 성향
-# 규칙1
-# IF 안전도가 / 낮다
-# and 수익률이 / 높다
-# then 비중이 / 낮다
-#
-# 규칙2
-# IF 안전도가 / 높다
-# and 수익률이 / 낮다
-# then 비중이 / 보통이다
-#
-# 규칙3
-# IF 안전도가 / 할만하다
-# and 수익률이 / 높다
-# then 비중이 / 높다
-#########################################
-
-setHigh=[]
-for index, x in fuzzyDf.iterrows():
-    setHigh.append(x['SafetyMid']*x['returnHigh'])
-fuzzyDf['setHigh']=setHigh
-
-setMid=[]
-for index, x in fuzzyDf.iterrows():
-    setMid.append(x['SafetyHigh']*x['returnLow'])
-fuzzyDf['setMid']=setMid
-
-setLow=[]
-for index, x in fuzzyDf.iterrows():
-    setLow.append(x['SafetyLow']*x['returnHigh'])
-fuzzyDf['setLow']=setLow
+propensity = 'moderate'
+fuzzyDf['setHigh'],fuzzyDf['setMid'],fuzzyDf['setLow'] = RuleCalc(propensity)
 
 ##############################################
-# COG 구할 차례
+# Defuzzification uses COG
 ##############################################
+fuzzyDf['COG']=\
+fuzzyDf['setLow']*(10+20)+fuzzyDf[['setMid','setLow']].max(axis=1)*(30+40)\
++fuzzyDf['setMid']*50+fuzzyDf[['setHigh','setMid']].max(axis=1)*(60+70)+fuzzyDf['setHigh']*(80+90+100)
 
-fuzzyDf
+resultDf['percentage']=(fuzzyDf['COG']/fuzzyDf['COG'].sum()).round(2)
+resultReturn = resultDf['기대수익률']*resultDf['percentage']
+print (pandas.DataFrame(resultDf[['소유형','기대수익률','percentage']]))
+
+print (propensity, 'expected return:',resultReturn.sum()) # expected return
